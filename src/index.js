104259d2c041d3bcf239ecff0bc19d1e58ca90c7
mmfilesi <mmfilesi@gmail.com>
2017-05-02 22:42:16 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+/*
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+*/
+
+ReactDOM.render(
+  <h3>Hola Mundo</h3>,
+  document.getElementById('root')
+);
